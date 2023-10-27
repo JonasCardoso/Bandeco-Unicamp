3ddc653531cd9e469a2111b54e9e8c329f42d8a7
Jonas Cardoso <jonascard60@gmail.com>
2023-10-27 17:18:39 -0300

    telegram_servico: add de volta try e catch

--- src/telegram_servico.py
+++ src/telegram_servico.py
@@ -5,19 +5,31 @@ from telegram.ext import CallbackContext
 
 async def mandar_mensagem(context: CallbackContext, chat_id, texto, reply_markup=None, reply_to_message_id=None,
                           parse_mode="Markdown"):
-    await context.bot.send_message(chat_id=chat_id, parse_mode=parse_mode, text=texto, reply_markup=reply_markup,
-                                   reply_to_message_id=reply_to_message_id)
+    try:
+        await context.bot.send_message(chat_id=chat_id, parse_mode=parse_mode, text=texto, reply_markup=reply_markup,
+                                       reply_to_message_id=reply_to_message_id)
+    except:
+        None
 
 
 async def deletar_mensagem(context: CallbackContext, chat_id, message_id):
-    await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+    try:
+        await context.bot.delete_message(chat_id=chat_id, message_id=message_id)
+    except:
+        None
 
 
 async def mandar_mensagem_teclado(context: CallbackContext, chat_id, texto, buttons):
-    await context.bot.send_message(chat_id=chat_id, parse_mode="Markdown", text=texto,
-                                   reply_markup=ReplyKeyboardMarkup(buttons))
+    try:
+        await context.bot.send_message(chat_id=chat_id, parse_mode="Markdown", text=texto,
+                                       reply_markup=ReplyKeyboardMarkup(buttons))
+    except:
+        None
 
 
 async def mandar_imagem(context: CallbackContext, chat_id, imagem):
-    await context.bot.send_photo(chat_id=chat_id, parse_mode="Markdown",
-                                 photo=open(f'{pathlib.Path().resolve()}/{imagem}.jpg', 'rb'))
+    try:
+        await context.bot.send_photo(chat_id=chat_id, parse_mode="Markdown",
+                                     photo=open(f'{pathlib.Path().resolve()}/{imagem}.jpg', 'rb'))
+    except:
+        None
